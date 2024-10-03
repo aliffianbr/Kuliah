6981173ed3797bbc09709e97cbcbebcf1c436b8c
aliffianbr <131659292+aliffianbr@users.noreply.github.com>
2024-10-03 21:16:03 +0700

    Add files via upload

---
+++ Rekayasa-Web/1_arraytoJson_G211220122.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = array(
+    "nama" => "Aliffian Akbar Hariyanto",
+    "usia" => 18,
+    "pekerjaan" => "Mahasiswa",
+    "NIM" => "G.211.22.0122"
+);
+
+$json_data = json_encode($data);
+
+echo $json_data;
+?>
